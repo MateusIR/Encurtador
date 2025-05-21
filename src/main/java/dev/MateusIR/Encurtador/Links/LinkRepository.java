7b139384a90d7b06290eb2a3bc6002d784167d9c
Mateus ieno <125301838+MateusIR@users.noreply.github.com>
2025-05-21 16:10:20 -0300

    Cria interface `LinkRepository`

---
+++ src/main/java/dev/MateusIR/Encurtador/Links/LinkRepository.java
@@ -0,0 +1,10 @@
+package dev.MateusIR.Encurtador.Links;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+
+
+
+}
