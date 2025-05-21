48d61478fe197e6a28ee4aa21fd82271abf9f85a
Mateus ieno <125301838+MateusIR@users.noreply.github.com>
2025-05-21 14:37:28 -0300

    first commit

---
+++ src/test/java/dev/MateusIR/Encurtador/EncurtadorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.MateusIR.Encurtador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
